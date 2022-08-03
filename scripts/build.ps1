38e486524755cce31fb49afa8aef7d2fc3a11634
Andreas Geiß <andreas.geiss@atruvia.de>
2022-08-03 15:01:13 +0200

    refactoring and cleanup

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,6 @@
 
 protoc --go_out=. --go-grpc_out=. .\api\hello.proto
 
-go build -ldflags "-s -w" -o grpc-test.exe .\cmd\cli\main.go
+go build -ldflags "-s -w" -o hello-client.exe .\cmd\hello-client\main.go
+go build -ldflags "-s -w" -o hello-local.exe .\cmd\hello-local\main.go
+go build -ldflags "-s -w" -o hello-server.exe .\cmd\hello-server\main.go
